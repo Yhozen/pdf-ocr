9cd2ba090af557f2a576f19cadedc260ca405df2
Gabriel Perez <gabriel@garox.org>
2022-04-17 12:57:47 -0400

    read from image

--- app/main.py
+++ app/main.py
@@ -1,1 +1,33 @@
+import os
+
+from tesserocr import image_to_text
+from pdf2image import convert_from_path
+from tqdm import tqdm
+
+import tempfile
+
+# Do something here
+
+curr_directory = os.getcwd()
+
+
+def lectura_contratos(contrato):
+    """
+    It takes a PDF file, converts it to a series of images, and saves them in a folder
+
+    :param output: The directory where you want to save the images
+    :param contrato: The path to the PDF file you want to convert
+    """
+    full_text = ''
+    pdfs = os.path.join(curr_directory, "input", contrato)
+    with tempfile.TemporaryDirectory() as path:
+        images_from_path = convert_from_path(
+            pdfs, 350, output_folder=path)
+
+        for page in tqdm(images_from_path):
+            full_text += image_to_text(page, lang='spa')
+    return full_text
+
+
+print(lectura_contratos("input.pdf"))
 print("hello")
